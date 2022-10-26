e3f7a7bc042009ba2618bb5e441ea4b6410e80fd
Liam Ronan <liamronan16@gmail.com>
2022-10-26 23:24:30 +0100

    finished components

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -36,15 +36,16 @@
                     <li>
                         <a href="login.html" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i>Login</a>
                     </li>
+                    <li>
+                        <a href="create.html" class="hover:text-laravel"><i class="fa-solid fa-upload"></i>Post Project</a>
+                    </li>
                 </ul>
             </nav>
         <main>
             @yield('content')
         </main>
-        <footer class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
-            <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
-
-            <a href="create.html" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Post Project</a>
+        <footer class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-black text-white h-24 mt-24 opacity-90 md:justify-center">
+            <a href="create.html" class="absolute center-1/3 center-5 bg-black text-white py-2 px-5">Post Project</a>
         </footer>
     </body>
 </html>
