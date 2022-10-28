8682f4ed445215204dd8bf30d6ebe2060805aae0
Liam Ronan <liamronan16@gmail.com>
2022-10-28 01:40:57 +0100

    logo added

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -30,7 +30,7 @@
     </head>
     <body class="mb-48">
             <nav class="flex justify-between items-center mb-6 p-4">
-                <a href="/"><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo"/></a>
+                <a href="/"><img class="w-24" src="{{asset('images/ales-nesetril-Im7lZjxeLhg-unsplash.jpg')}}" alt="" class="logo"/></a>
                 <ul class="flex space-x-4 mr-4 text-lg">
                     <li>
                         <a href="register.html" class="hover:text-laravel"><i class="fa-solid fa-user-plus p-1"></i>Register</a>
